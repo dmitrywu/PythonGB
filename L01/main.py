36ae76b747fa945483b0b4a15f3ebc3477a69235
Dmitry <dmitry.wu@gmail.com>
2023-11-29 02:29:52 +0300

    tezt

---
+++ L01/main.py
@@ -0,0 +1,31 @@
+
+
+# print('Input number')
+# a=int(input())
+# b=int(input('Input 2-num'))
+# print(a+b)
+
+# a=7.34
+# b=int(a)
+# c=1
+# d=bool(c)
+# print(f'{a} - {b} - {c} - {d}')
+
+# print('Input number')
+# a= input()
+# b=input('Input 2-num')
+# print(a+b)
+
+# a=5
+# c=1.324
+# b='test'
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+
+# n = 5
+# m=None
+# o=3.12
+# #p='fd/'fd'
+# p='fd\'fd'
+# print(n,m,o,type(o), p)
+
