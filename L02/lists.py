a842a7ee53a0ce97c9386834ef4d51cb7194cdf4
Dmitry <dmitry.wu@gmail.com>
2023-12-13 14:47:56 +0300

    add L02 full src. Done.

--- L02/lists.py
+++ L02/lists.py
@@ -1,43 +1,56 @@
-list_1 = [12, 7, -1, 21, 0]
-print(list_1.insert(2,11))
+list_1 = [] # Создание пустого списка
+list_2 = list() # Создание пустого списка
+list_1 = [7, 9, 11, 13, 15, 17]
+print(list_1[0]) # 7
+print(list_1[-1]) # 17
 print(list_1)
+print(*list_1) #убираем квадратные скобки
 
+#append
+list_1.append(85) # add 85
+print(*list_1)
+print(len(list_1))
 
-# list_1 = [12, 7, -1, 21, 0]
-# print(list_1.pop(1))
-# print(list_1)
-
-# list_1 = [12, 7, -1, 21, 0]
+#pop - удаление элемента, по умолчанию - последнего
 # print(list_1.pop())
-# print(list_1)
-# print(list_1.pop())
-# print(list_1)
-# print(list_1.pop())
-# print(list_1)
-
-
-# list_1=[]
-# print(list_1)
-# for i in range(5):
-#     list_1.append(i)
-#     print(list_1)
-
-
-# list_1 =[1,5]
-# print(list_1)
-# list_1.append(8)
-# print(list_1)
-# list_1.append(45)
-# print(list_1)
-
-# list_1 = []
-# list_1 = list()
-# list_1 = [1, 2, 3, 8]
-# print(len(list_1))
-# print(list_1[2])
-# print(list_1)
 # print(*list_1)
+# print(len(list_1))
+# print(list_1.pop())
+# print(*list_1)
+# print(len(list_1))
 
+#или с определённым индексом
+print(list_1.pop(2))
+print(*list_1)
+print(len(list_1))
 
-# for i in list_1:
-#     print(i)
+#insert
+
+print(list_1.insert(2,42))
+print(*list_1)
+print(len(list_1))
+
+#срезы списка
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_1[0])
+# 1
+print(list_1[1])
+# 2
+print(list_1[len(list_1)-1])
+# 10
+print(list_1[-5])
+# 6
+print(list_1[:])
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_1[:2])
+# [1, 2]
+print(list_1[len(list_1)-2:])
+#[9, 10]
+print(list_1[2:9])
+# [3, 4, 5, 6, 7, 8, 9]
+print(list_1[6:-18])
+# []
+print(list_1[0:len(list_1):6])
+# [1, 7]
+print(list_1[::6])
+# [1, 7
---
+++ L02/tuple.py
@@ -0,0 +1,25 @@
+t = () # создание пустого кортежа
+print(type(t)) # class <'tuple'>
+t = (1,)
+print(type(t))
+t = (1)
+print(type(t))
+t = (28, 9, 1990)
+print(type(t))
+colors = ['red', 'green', 'blue']
+print(colors)
+# ['red', 'green', 'blue']
+t = tuple(colors)
+print(t)
+# ('red', 'green', 'blue')
+t = tuple(['red', 'green', 'blue'])
+print(t[0]) # red
+print(t[2]) # blue
+for e in t:
+    print(e) # red green blue
+#t[0] = 'black' # TypeError: 'tuple' object does not support(нельзя изменять кортеж
+
+#Распаковка кортежа
+t = tuple(['red', 'green', 'blue'])
+red, green, blue = t
+print('r:{} g:{} b:{}'.format(red, green, blue)) # r:red g:green b:blue
